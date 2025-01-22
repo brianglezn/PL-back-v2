82e877541e8b325a3317710bf2f7ba87cdcf743e
Brian González Novoa <119687340+brianglezn@users.noreply.github.com>
2025-01-22 19:44:58 +0100

    Cambio de front para CORS

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000;
 // Enable Cross-Origin Resource Sharing (CORS) for specific origins
 app.use(cors({
     origin: (origin, callback) => {
-        const allowedOrigins = ['https://v2.profit-lost.com', 'http://localhost:5173'];
+        const allowedOrigins = ['https://profit-lost.com', 'http://localhost:5173'];
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
